3d484cd96be3902d4ac01ed0f2f5d9ab4a3f82e6
AshleyEmily <73400786+AshleyEmily@users.noreply.github.com>
2021-08-13 00:05:25 +0800

    Update script.js

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -83,7 +83,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  function(whatever){
+    buildAndShowHomeHTML(whatever);
+  }, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
